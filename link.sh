eaabb5eed16f782ba4af300af12f7131cecd3e7c
hisakot <t-hisako@keio.jp>
2019-09-23 20:05:13 +0900

    only file symboliclink

--- link.sh
+++ link.sh
@@ -9,8 +9,25 @@ cd $THIS_DIR
 echo "start setup..."
 for f in .??*; do
 	[ "${f}" = ".git" ] && continue
+	[ "${f}" = ".config" ] && continue
 
 	ln -snfv $HOME/dotfile/${f} $HOME/${f}
+done
+
+ln -snfv $HOME/dotfile/.config/nvim/init.vim $HOME/.config/nvim/init.vim
+cd $THIS_DIR/.config/nvim/dein
+for f in *; do
+	ln -snfv $HOME/dotfile/.config/nvim/dein/${f} $HOME/.config/nvim/dein/${f}
+done
+
+cd $THIS_DIR/.config/autostart
+for f in *; do
+	ln -snvf $HOME/dotfile/.config/autostart/${f} $HOME/.config/autostart/${f}
+done
+
+cd $THIS_DIR/.config/evince
+for f in *; do
+	ln -snvf $HOME/dotfile/.config/evince/${f} $HOME/.config/evince/${f}
 done
 
 cat << END
